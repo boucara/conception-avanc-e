2857669cf15509e26508e510b552fca70ee973d4
boucara <traorebelco@gmail.com>
2017-09-20 01:01:29 +0200

    fin d'implementation du sprint1  VERSION1

--- AppliVentesStocks/src/controleur/TraitementActionClient.java
+++ AppliVentesStocks/src/controleur/TraitementActionClient.java
@@ -1,5 +1,39 @@
 package controleur;
 
-public class TraitementActionClient {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import vue.PageAcceuil;
+
+public class TraitementActionClient extends AbstractAction  {
+private PageAcceuil fenetre;
+	
+	public  TraitementActionClient(PageAcceuil fenetre, String texte){
+		super(texte);
+		
+		this.fenetre = fenetre;
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		String pseudoUser = fenetre.getTextField().getText();
+		JFrame fenetreLogin = new JFrame("Application Ventes et Stocks");
+		JLabel label = new JLabel("", JLabel.BOTTOM);
+		
+		label.setText("Bienvenue sur notre site de  Ventes " + pseudoUser);
+		fenetreLogin.add(label);
+		fenetreLogin.setLocationRelativeTo(null);
+		fenetreLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetreLogin.setSize(500, 500);
+		fenetreLogin.setVisible(true);
+		
+	
+		
+	}
 
 }
--- AppliVentesStocks/src/vue/PageAcceuil.java
+++ AppliVentesStocks/src/vue/PageAcceuil.java
@@ -1,34 +1,60 @@
 package vue;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
-public class PageAcceuil  extends JFrame{
-	 JLabel headerLabel ;
-	 JPanel  controlPanel ;
-	 public PageAcceuil(){
-		    this.setTitle("Application Ventes et Stoks");
-		    headerLabel = new JLabel("French Chic", JLabel.CENTER); 
-		    //headerLabel.setSize(1000,500);
-		    Font font = new Font("Arial",Font.BOLD,36);
-		    headerLabel.setFont(font);
+import controleur.TraitementActionClient;
 
-		   // headerLabel.setForeground(Color.pink);
-		    controlPanel = new JPanel();
-		    controlPanel.setLayout(new FlowLayout());
+public class PageAcceuil extends JFrame {
+	JLabel headerLabel, contentLabel;
+	JPanel controlPanel;
+	JTextField txtEmail;
 
-		    controlPanel.add(headerLabel);
-		    this.add(controlPanel);
-		    
-		    this.setSize(1000, 500);
-		    this.setLocationRelativeTo(null);
-		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-		    this.setVisible(true);
-		  }
+	public PageAcceuil() {
+		this.setTitle("Application Ventes et Stoks");
+		headerLabel = new JLabel("French Chic", JLabel.CENTER);
+		// headerLabel.setSize(1000,500);
+		Font font = new Font("Arial", Font.BOLD, 36);
+		headerLabel.setFont(font);
+		headerLabel.setForeground(Color.pink);
+
+		JPasswordField password = new JPasswordField("Mot de passe");
+		 txtEmail = new JTextField("Pseudo");
+
+		txtEmail.setPreferredSize(new Dimension(200, 24));
+		JButton bouton = new JButton(new TraitementActionClient(this, "S'identifier"));
+
+		
+
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(5, 4, 7, 7));
+		p.setBackground(Color.white);
+
+		p.add(headerLabel);
+		p.add(txtEmail);
+		p.add(password);
+		p.add(bouton);
+
+		this.add(p);
+
+		this.setSize(1000, 500);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	public JTextField getTextField(){
+		return txtEmail;
+	}
+	
 
 }
