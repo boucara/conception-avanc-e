4a37f5be553179f13f9f43826791b0166baf4c5e
boucara <traorebelco@gmail.com>
2017-09-19 23:40:53 +0200

    implementation du sprint 1

--- AppliVentesStocks/src/metier/Client.java
+++ AppliVentesStocks/src/metier/Client.java
@@ -1,5 +1,23 @@
 package metier;
 
 public class Client {
+	private String pseudo;
+	private String motDePasse ;
+	public  Client(String pseudo , String motDePasse) {
+		this.setPseudo(pseudo) ;
+		this.setMotDePasse(motDePasse) ;
+	}
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
 
 }
--- AppliVentesStocks/src/vue/PageAcceuil.java
+++ AppliVentesStocks/src/vue/PageAcceuil.java
@@ -1,5 +1,34 @@
 package vue;
 
-public class PageAcceuil {
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PageAcceuil  extends JFrame{
+	 JLabel headerLabel ;
+	 JPanel  controlPanel ;
+	 public PageAcceuil(){
+		    this.setTitle("Application Ventes et Stoks");
+		    headerLabel = new JLabel("French Chic", JLabel.CENTER); 
+		    //headerLabel.setSize(1000,500);
+		    Font font = new Font("Arial",Font.BOLD,36);
+		    headerLabel.setFont(font);
+
+		   // headerLabel.setForeground(Color.pink);
+		    controlPanel = new JPanel();
+		    controlPanel.setLayout(new FlowLayout());
+
+		    controlPanel.add(headerLabel);
+		    this.add(controlPanel);
+		    
+		    this.setSize(1000, 500);
+		    this.setLocationRelativeTo(null);
+		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+		    this.setVisible(true);
+		  }
 
 }
