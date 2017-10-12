2de083d253e6c98a2b3a1e77d2d95c37c6ec689b
Michaël Gianfaldone <m.gianfaldone@gmail.com>
2017-10-12 08:57:52 +0200

    AffichagePanier()

--- AppliVentesStocks/src/vue/VueJetable.java
+++ AppliVentesStocks/src/vue/VueJetable.java
@@ -5,9 +5,12 @@ package vue;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -16,10 +19,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
 import controleur.EnumTypeEcran;
 import controleur.Session;
+import metier.Produit;
 
 public class VueJetable extends JFrame {
 	
@@ -119,12 +125,13 @@ public class VueJetable extends JFrame {
 
 	}
 	
-	public void affichageEcranPanier() {
+	public void affichageEcranPanier(HashMap<Produit, Integer> panierClient) {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		
 		this.add(panel);
+		panel.setLayout(new BorderLayout());
 		
 		JLabel headerLabel = new JLabel("Votre Panier", JLabel.LEFT);
 		Font font = new Font("Arial", Font.PLAIN, 43);
@@ -132,8 +139,63 @@ public class VueJetable extends JFrame {
 		headerLabel.setForeground(new Color(206, 0, 206));
 		headerLabel.setAlignmentX(this.LEFT_ALIGNMENT);
 		
-		panel.add(headerLabel);
+		JPanel p1 = new JPanel();
 		
+		String[] nomCols = {"Libellé", "Prix HT", "Quantité", "Montant"};
+		
+		Object[][] datas = new Object[panierClient.size()][4];
+		
+		int i = 0;
+		int j = 0;
+		// Génération des données pour le tableau
+		for(Entry<Produit, Integer> entry : panierClient.entrySet()) {
+			
+			Produit produitDuPanier = entry.getKey();
+			Integer quantite = entry.getValue();
+			
+			datas[i][0] = produitDuPanier.getLabel();
+			datas[i][1] = Double.toString(produitDuPanier.getPrixHT());
+			datas[i][2] = quantite.toString();
+			datas[i][3] = Double.toString(quantite.doubleValue() * produitDuPanier.getPrixHT());
+			
+			i++;
+		}
+		
+		JTable table = new JTable(datas, nomCols);
+		table.setCellSelectionEnabled(false);
+		table.setDragEnabled(false);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(table);
+		scrollPane.setPreferredSize(new Dimension(700, 500));
+		table.setGridColor(new Color(206, 0, 206));
+		table.setBackground(Color.LIGHT_GRAY);
+		table.setBorder(BorderFactory.createLineBorder(new Color(206, 0, 206)));
+		panel.add(headerLabel, BorderLayout.NORTH);
+		panel.add(scrollPane, BorderLayout.CENTER);
+		
+		p1.setBackground(Color.WHITE);
+		
+		Box box = Box.createVerticalBox();
+		box.add(Box.createVerticalStrut(30));
+		
+		JLabel labelMontant = new JLabel("Montant panier");
+		JTextField montant = new JTextField();
+		montant.setPreferredSize(new Dimension(50, 20));
+		montant.setAlignmentX(this.RIGHT_ALIGNMENT);
+		JPanel p2 = new JPanel();
+		p2.setLayout(new FlowLayout(FlowLayout.LEADING, 50, 0));
+		p2.setBorder(BorderFactory.createEmptyBorder(0, 100, 0, 100));
+		p2.add(labelMontant);
+		p2.add(montant);
+		p2.setSize(150, 30);
+		p2.setBackground(Color.WHITE);
+		box.add(p2);
+		
+		panel.add(box, BorderLayout.SOUTH);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(600, 350);
 		this.setVisible(true);
 		
 	}
@@ -144,9 +206,11 @@ public class VueJetable extends JFrame {
 		
 		Session controleur  = new Session();
 		VueJetable vue=new VueJetable();
-		vue.affichageEcranPanier();
+		HashMap<Produit, Integer>panier = new HashMap<Produit, Integer>();
+		//vue.affichageEcranPanier(panier);
+		
 		if(controleur.traitementConnexion()==EnumTypeEcran.ECRAN_ACCUEIL) {
-			vue.affichageEcranAccueil();;
+			vue.affichageEcranAccueil();
 		}
 		if(controleur.traitementConnexion()==EnumTypeEcran.ECRAN_PERSO) {
 			
